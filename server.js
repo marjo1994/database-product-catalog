e6fb07d7959156a17859427240397bf7514c3b13
marjo1994 <m_rhl@live.com>
2025-03-05 17:49:30 -0500

    Pagination - Products

--- server.js
+++ server.js
@@ -5,6 +5,37 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+// Custom pagination logic
+server.get('/products', (req, res) => {
+const page = parseInt(req.query._page) || 1;
+const limit = parseInt(req.query._per_page) || 8;
+  
+  // Get data from the db
+  const db = router.db;
+  const products = db.get('products').value();
+
+  const totalItems = products.length;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  // Get the items for the current page
+  const paginatedProducts = products.slice((page - 1) * limit, page * limit);
+
+  // Add pagination metadata
+  const pagination = {
+    first: 1,
+    prev: page > 1 ? page - 1 : null,
+    next: page < totalPages ? page + 1 : null,
+    last: totalPages,
+    pages: totalPages,
+    items: totalItems,
+  };
+
+  res.setHeader('X-Total-Count', totalItems);
+  res.jsonp({ pagination, items: paginatedProducts });
+});
+
+server.use(router);
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
